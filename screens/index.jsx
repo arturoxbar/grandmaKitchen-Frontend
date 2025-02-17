1495dee933bbf4b2225fd53fad9cbc5ed9950d3d
arturoxbar <101169728+arturoxbar@users.noreply.github.com>
2025-02-16 20:41:17 -0400

    added createEdit view

--- screens/index.jsx
+++ screens/index.jsx
@@ -1,5 +1,7 @@
 import Login from "./Login";
 import Signup from "./Signup";
 import Welcome from "./Welcome";
+import Home from "./Home";
+import CreateEdit from "./CreateEdit";
 
-export { Login, Signup, Welcome };
+export { Login, Signup, Welcome, Home, CreateEdit };
