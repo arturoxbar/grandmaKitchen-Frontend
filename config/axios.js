c04c15a405b8622ea61e0b21ef1c4cd9e70f3874
arturoxbar <101169728+arturoxbar@users.noreply.github.com>
2025-02-18 02:22:21 -0400

    created axios instance

---
+++ config/axios.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const AxiosInstance = axios.create({
+  baseURL: "http://192.168.1.117:7338/api/v1",
+  timeout: 2000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const UserEndpoints = {
+  create: "/users/signup",
+  login: "/users/login",
+};
+
+export { AxiosInstance, UserEndpoints };
