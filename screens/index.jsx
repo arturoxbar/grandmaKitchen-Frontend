d15b6b50bd2fb7031a363b087db030d4b2e92b2d
arturoxbar <101169728+arturoxbar@users.noreply.github.com>
2025-02-20 01:48:56 -0400

    Update index.jsx

--- screens/index.jsx
+++ screens/index.jsx
@@ -3,5 +3,17 @@ import Signup from "./Signup";
 import Welcome from "./Welcome";
 import Home from "./Home";
 import CreateEdit from "./CreateEdit";
+import RecipeDetails from "./Details";
+import User from "./User";
+import RecoverPassword from "./RecoverPassword";
 
-export { Login, Signup, Welcome, Home, CreateEdit };
+export {
+  Login,
+  Signup,
+  Welcome,
+  Home,
+  CreateEdit,
+  RecipeDetails,
+  User,
+  RecoverPassword,
+};
